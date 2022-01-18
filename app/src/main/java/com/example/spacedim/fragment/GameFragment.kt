1d2a7009c8f6c477fd1caaec50c8db3bbb68a3b4
Emma <emma.da29@orange.fr>
2022-01-18 08:55:30 +0100

    Fix import

--- app/src/main/java/com/example/spacedim/fragment/GameFragment.kt
+++ app/src/main/java/com/example/spacedim/fragment/GameFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.spacedim.classes.UIElement
